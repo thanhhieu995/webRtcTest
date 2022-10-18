3daa09291dc89cc686069196992df22434b7497d
Hieu <thanhhieu995@gmail.com>
2022-10-18 16:45:57 +0700

    Fix size children in main.xml

---
+++ app/src/main/java/vn/videocall/webrtctest/SignupActivity.kt
@@ -0,0 +1,12 @@
+package vn.videocall.webrtctest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.webrtctest.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
