37eb5d2962694efe547cfdad5016d02e6720174e
Hieu <thanhhieu995@gmail.com>
2022-10-18 17:49:26 +0700

    Create plash

--- app/src/main/java/vn/hieunguyen1/appvideo/LoginActivity.kt
+++ app/src/main/java/vn/hieunguyen1/appvideo/LoginActivity.kt
@@ -1,5 +1,7 @@
 package vn.hieunguyen1.appvideo
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, LoginActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/vn/hieunguyen1/appvideo/PlashActivity.kt
+++ app/src/main/java/vn/hieunguyen1/appvideo/PlashActivity.kt
@@ -2,10 +2,17 @@ package vn.hieunguyen1.appvideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class PlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plash)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            LoginActivity.start(this)
+        }, 500)
     }
 }
