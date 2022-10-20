51f36ca41e6f1d84f23c52b8e96a0b76116ce7ad
Hieu <thanhhieu995@gmail.com>
2022-10-20 15:20:20 +0700

    Check enter name is string and not contain specific element.

--- app/src/main/java/vn/hieunguyen1/appvideo/SignupActivity.kt
+++ app/src/main/java/vn/hieunguyen1/appvideo/SignupActivity.kt
@@ -1,12 +1,18 @@
 package vn.hieunguyen1.appvideo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import java.util.Scanner
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -34,28 +40,38 @@ class SignupActivity : AppCompatActivity() {
         return !(edtName.text.isNullOrEmpty() || edtEmail.text.isNullOrEmpty() || edtPassword.text.isNullOrEmpty())
     }
 
-    fun signUp() {
+    fun checkString() : Boolean {
+        return edtName.text.contains("[a-zA-Z]+") && edtName.text.length > 2 && !edtName.text.contains("[!@#$%^&*()_+=<>,.?/|]")
+    }
+
+
+    private fun signUp() {
         btnSigUp.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 if (checkEmpty()) {
-                    mAuth.createUserWithEmailAndPassword(
-                        edtEmail.text.toString(),
-                        edtPassword.text.toString()
-                    ).addOnCompleteListener { it
-                        if (it.isSuccessful) {
+                    if (checkString()) {
+                        mAuth.createUserWithEmailAndPassword(
+                            edtEmail.text.toString(),
+                            edtPassword.text.toString()
+                        ).addOnCompleteListener {
+                            it
+                            if (it.isSuccessful) {
 
-                            Toast.makeText(
-                                this@SignupActivity,
-                                "Sign up success",
-                                Toast.LENGTH_LONG
-                            ).show()
-                        } else {
-                            Toast.makeText(
-                                this@SignupActivity,
-                                "Sign up fail",
-                                Toast.LENGTH_LONG
-                            ).show()
+                                Toast.makeText(
+                                    this@SignupActivity,
+                                    "Sign up success",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            } else {
+                                Toast.makeText(
+                                    this@SignupActivity,
+                                    "Sign up fail",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
                         }
+                    } else {
+                        Toast.makeText(this@SignupActivity, "Please don't enter number and more than two alphabet", Toast.LENGTH_LONG).show()
                     }
                 } else {
                     Toast.makeText(
