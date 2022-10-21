3daa3db6c28ae109e6b244fa7cf450b6d94b586f
Hieu <thanhhieu995@gmail.com>
2022-10-21 09:49:05 +0700

    Add feature show/hide password in LoginActivity.

--- app/src/main/java/vn/hieunguyen1/appvideo/LoginActivity.kt
+++ app/src/main/java/vn/hieunguyen1/appvideo/LoginActivity.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,6 +18,8 @@ class LoginActivity : AppCompatActivity() {
     lateinit var txtPassword: EditText
     lateinit var btnLogin: Button
     lateinit var btnSignup: Button
+    lateinit var eyePassword: ImageView
+    var isPress: Boolean = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +30,8 @@ class LoginActivity : AppCompatActivity() {
         txtPassword = findViewById(R.id.lg_edt_password)
         btnLogin = findViewById(R.id.lg_btn_login)
         btnSignup = findViewById(R.id.lg_btn_signup)
+
+        eyePassword = findViewById(R.id.lg_eye_password)
 
 //        btnSignup.setOnClickListener { OnClickListener {
 //            val intent = Intent(this@LoginActivity, SignupActivity::class.java)
@@ -39,6 +46,19 @@ class LoginActivity : AppCompatActivity() {
                 finish()
             }
 
+        })
+
+        eyePassword.setOnClickListener(object : OnClickListener{
+            override fun onClick(p0: View?) {
+                if (isPress) {
+                    eyePassword.setImageResource(R.drawable.ic_baseline_visibility_24)
+                    txtPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                } else {
+                    eyePassword.setImageResource(R.drawable.ic_baseline_visibility_off_24)
+                    txtPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                }
+                isPress = !isPress
+            }
         })
     }
 
